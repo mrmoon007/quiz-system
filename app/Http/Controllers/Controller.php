8735b6eba79ee82e3c593e32f7ec0b0ddca215f3
mostafijur <mrmoon025@gmail.com>
2024-08-22 09:33:31 +0600

    [feat] Setup swagger for API documentation.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Quiz System Documentation",
+ *      description="Quiz System description",
+ *      @OA\Contact(
+ *          email="mostafijur.til@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     path="/api/Home",
+     *     operationId="getProfile",
+     *     tags={"Profile"},
+     *     summary="Get user profile",
+     *     description="Retrieve user profile information.",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="Authorization",
+     *         in="header",
+     *         description="Authorization Token",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             default="Bearer your_access_token_here"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent()
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     )
+     * )
+     */
+
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
